f6137e414b6c94ad6c5991ca105ab96a0f1c72dc
Sam Huynh <samhwang2112.dev@gmail.com>
2021-12-09 22:09:42 +1100

    Add command line script to read input and options

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
-import { parseCSVInput, convertBWTo1P, write1PasswordCSV } from './bw-to-1p';
+import { Command } from 'commander';
+import {
+    getRelativeFilepath,
+    parseCSVInput,
+    convertBWTo1P,
+    write1PasswordCSV,
+} from './bw-to-1p';
 
-const input = '../input/sample.csv';
-const output = '../output/test.csv';
+const program = new Command();
+program
+    .version('0.0.1', '-v', 'output current version')
+    .name('bw-to-1p')
+    .description('Convert BitWarden logins into valid 1Password CSVs')
+    .option('-i, --input [input]', 'input file', 'input/sample.csv')
+    .option('-o, --output [output]', 'output file', 'output/out.csv')
+    .parse();
 
-const go = () => {
-    const bwLogins = parseCSVInput(input);
-    const onePassLogins = convertBWTo1P(bwLogins);
-    write1PasswordCSV(onePassLogins, output);
-};
+const { input: inputFile, output: outputFile } = program.opts();
 
-go();
+try {
+    console.log(`READING INPUT FILE: ${inputFile}`);
+    const inputPath = getRelativeFilepath(inputFile);
+    const BWLogins = parseCSVInput(inputPath);
+
+    console.log(`CONVERTING TO 1PASSWORD LOGINS`);
+    const onePassLogins = convertBWTo1P(BWLogins);
+
+    console.log(`WRITING TO OUTPUT FILE: ${outputFile}`);
+    write1PasswordCSV(onePassLogins, getRelativeFilepath(outputFile));
+
+    console.log(`WRITE DONE!`);
+} catch (error: any) {
+    console.error(`ENCOUNTERED ERROR: ${error}`);
+}
